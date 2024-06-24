27617e51baef86668f4446fe7a7a940329a2e565
Shadow9916 <136230860+Shadow9916@users.noreply.github.com>
2024-06-24 18:17:25 +0000

    Shadow

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "SHADOW_LAWAL"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SHADOW"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SD"
 )
 
 var ID ids.ID
